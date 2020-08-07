5017918d60266b3a24d0c0519d72d9fa995f9662
7nic7 <714825233@qq.com>
2020-08-07 15:41:13 +0800

    fib unittest

---
+++ python/unit_test/fib/fib.py
@@ -0,0 +1,12 @@
+
+def fib(n):
+    if n < 0:
+        raise ValueError('请输入一个正数')
+    if n == 1:
+        return [1]
+    if n == 2:
+        return [1, 1]
+    fibs = [1, 1]
+    for i in range(2, n):
+        fibs.append(fibs[i - 1] + fibs[i - 2])
+    return fibs
