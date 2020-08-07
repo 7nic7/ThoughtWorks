b01d0f095d73faf57976c29d584e9b73e42786db
7nic7 <714825233@qq.com>
2020-08-07 14:37:56 +0800

    unittest

--- python/unit_test/rect.py
+++ python/unit_test/rect.py
@@ -8,9 +8,9 @@ class Rect:
 
     def intersect(self, rect: 'Rect') -> bool:
         # TODO: implement me
-        if rect.y > self.y + self.height or \
-                rect.y + rect.height < self.y or \
-                rect.x > self.x + self.width or \
-                rect.x + rect.width < self.x:
+        if rect.y >= self.y + self.height or \
+                rect.y + rect.height <= self.y or \
+                rect.x >= self.x + self.width or \
+                rect.x + rect.width <= self.x:
             return False
         return True
--- python/unit_test/rect_test.py
+++ python/unit_test/rect_test.py
@@ -7,6 +7,11 @@ class RectTest(unittest.TestCase):
     def test_intersect_for_crossing_rects(self):
         self.assertTrue(Rect(1, 1, 1, 1).intersect(Rect(1, 1, 0.5, 0.5)))
 
+    def test_nonintersect(self):
+        self.assertFalse(Rect(1, 1, 1, 1).intersect(Rect(-1, -1, 0.5, 0.5)))
+
+    def test_edge_rects(self):
+        self.assertFalse(Rect(1, 1, 1, 1).intersect(Rect(1, 0, 1, 1)))
 
 if __name__ == '__main__':
     unittest.main()
